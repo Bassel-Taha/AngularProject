cabb7f76c166295b92ed5cbd4f7f016c90765e74
Bassel taha <121397705+Bassel-Taha@users.noreply.github.com>
2024-01-19 18:12:52 +0200

    adding the bootstrap and adding the components of the header home sidebar and the footer to the componnents

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {HeaderComponent} from "./Components/header/header.component";
+import {FooterComponent} from "./Components/footer/footer.component";
+import {HomeComponent} from "./Components/home/home.component";
+import {SideBarComponent} from "./Components/side-bar/side-bar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent, HomeComponent, SideBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'AngularProject';
+  x =  15;
+  y = 15;
+  z = this.x+this.y;
 }
