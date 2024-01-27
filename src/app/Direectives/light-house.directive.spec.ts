b5fe76ec2d78bf6abe3325ca443734bd3bdfb629
Bassel taha <121397705+Bassel-Taha@users.noreply.github.com>
2024-01-28 00:07:35 +0200

    -adding directive to make the image border change when hovering the mouse over the image and return to normal when the mouse is out

---
+++ src/app/Direectives/light-house.directive.spec.ts
@@ -0,0 +1,8 @@
+import { LightHouseDirective } from './light-house.directive';
+
+describe('LightHouseDirective', () => {
+  it('should create an instance', () => {
+    const directive = new LightHouseDirective();
+    expect(directive).toBeTruthy();
+  });
+});
