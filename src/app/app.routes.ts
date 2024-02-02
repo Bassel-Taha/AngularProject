dc3b4bbd0a301a0d394f2b8b6211d9f04a3ea7f6
Bassel taha <121397705+Bassel-Taha@users.noreply.github.com>
2024-02-03 00:22:51 +0200

    Adding the routing for all the components and making the home component the default component for the empty rout -using the children in the lay-out component to but the routing to routeroutlet in the layout component -and letting it be different from the routeroutlet in the App-Module html

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import {HomeComponent} from "./Components/home/home.component";
+import {OrderMasterComponent} from "./Components/order-master/order-master.component";
+import {ProductsListComponent} from "./Components/products-list/products-list.component";
+import {LayOutComponent} from "./Components/lay-out/lay-out.component";
+import {NotFoundComponent} from "./Components/not-found/not-found.component";
+import {LogInComponent} from "./Components/log-in/log-in.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:"login" , component: LogInComponent},
+  {path:"" , component: LayOutComponent , children:[
+      {path:"" , redirectTo:"/Home" , pathMatch:"full"},
+      {path:"Home", component:HomeComponent},
+      {path:"Order", component: OrderMasterComponent},
+      {path:"Products", component:ProductsListComponent},
+      {path:"**" , component:NotFoundComponent}
+    ]},
+];
