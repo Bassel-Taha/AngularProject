e42843fb71a72dd5b48306919ebed8838832a6f6
Bassel taha <121397705+Bassel-Taha@users.noreply.github.com>
2024-02-04 17:52:55 +0200

    using the ParamMap instead of the snapshot

--- src/app/Components/products-detials/products-detials.component.ts
+++ src/app/Components/products-detials/products-detials.component.ts
@@ -26,18 +26,21 @@ export class ProductsDetialsComponent implements OnInit{
   }
 
     ngOnInit(): void {
-     //when using snapshot in the ActivatedRout services if I want to navigate to the same component the angular won't run the OnInit and the page won't reload
+    /* //when using snapshot in the ActivatedRout services if I want to navigate to the same component the angular won't run the OnInit and the page won't reload
       this.productId = Number (this._activatedRoutService.snapshot.paramMap.get('ProductId'));
       this.selectedProduct = this._productsService.GetProductByProductId(this.productId!)
-      this.productCategory =  this._productsService.GetAllCategories().find(i=>i.ID == this.selectedProduct?.CategoryID)
+      this.productCategory =  this._productsService.GetAllCategories().find(i=>i.ID == this.selectedProduct?.CategoryID)*/
 
-      //todo "if there is a button that change the rout url and i need to get data from it to reload the same component over and over cuz angular wont run the OnInit if i changed to the rout to the component   "
-     /* // to get the page to reload with every change in the rout we mast use the paramMap directly and subscribe to it and get the data from it
+      //todo "if there is a function or an event that change the rout url and i need to get data from it to reload the same component over and over cuz angular wont run the OnInit if i changed to the rout to the component"
+      //
+      // to get the page to reload with every change in the rout we mast use the paramMap directly and subscribe to it and get the data from it
       //todo "and the rest of the functions must be put in the parentheses of the ParamMap"
-      this.productId = Number(this._activatedRoutService.paramMap.subscribe(paramMap => {paramMap.get('ProductId');
+      this._activatedRoutService.paramMap.subscribe(paramMap => {
+        this.productId= Number(paramMap.get("ProductId"));
         this.selectedProduct = this._productsService.GetProductByProductId(this.productId!);
         this.productCategory =  this._productsService.GetAllCategories().find(i=>i.ID == this.selectedProduct?.CategoryID);
-      }))*/
+        console.log(this.productId)
+      })
 
     }
 
