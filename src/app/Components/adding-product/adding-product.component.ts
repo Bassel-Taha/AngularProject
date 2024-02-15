b52870a224a5215169ca6066e16b7dc36b810020
Bassel taha <121397705+Bassel-Taha@users.noreply.github.com>
2024-02-15 19:24:24 +0200

    implementing the html and the logic for adding the product but still lack the logic to send the product to the api

--- src/app/Components/adding-product/adding-product.component.ts
+++ src/app/Components/adding-product/adding-product.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {IProduct} from "../../../Model/i-product";
+import {JsonPipe} from "@angular/common";
+import {FormsModule} from "@angular/forms";
+import {ProductsAPIServiceService} from "../../../Services/ProductsServiceAPI/products-apiservice.service";
+import {ICategories} from "../../../Model/ICategories";
 
 @Component({
   selector: 'app-adding-product',
   standalone: true,
-  imports: [],
+  imports: [
+    JsonPipe,
+    FormsModule
+  ],
   templateUrl: './adding-product.component.html',
   styleUrl: './adding-product.component.scss'
 })
-export class AddingProductComponent {
+export class AddingProductComponent implements OnInit{
 
+  newPrd: IProduct = {} as IProduct;
+  catList : ICategories[] = [] as ICategories[];
+
+  constructor(private _productsService : ProductsAPIServiceService) {
+  }
+
+  ngOnInit(): void {
+        this._productsService.GetAllCategories().subscribe(x=> this.catList = x);
+    }
 }
