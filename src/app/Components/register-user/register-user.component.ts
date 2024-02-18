8d0befb1fd0668bb75783b70ba963855fe8ef87b
Bassel taha <121397705+Bassel-Taha@users.noreply.github.com>
2024-02-18 22:18:27 +0200

    adding the form group in the TS of the registration component but still need to connect it to the DOM

--- src/app/Components/register-user/register-user.component.ts
+++ src/app/Components/register-user/register-user.component.ts
@@ -1,17 +1,42 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {HeaderComponent} from "../header/header.component";
-import {RouterLink} from "@angular/router";
+import {Router, RouterLink} from "@angular/router";
+import {FormBuilder, ReactiveFormsModule, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-register-user',
   standalone: true,
   imports: [
     HeaderComponent,
-    RouterLink
+    RouterLink,
+    ReactiveFormsModule
   ],
   templateUrl: './register-user.component.html',
   styleUrl: './register-user.component.scss'
 })
-export class RegisterUserComponent {
+export class RegisterUserComponent implements OnInit{
+
+  private RegistrationForm : any;
+  constructor(private router : Router, private _FB: FormBuilder) {
+    this.RegistrationForm = _FB.group({
+      'username': ['', [Validators.required , Validators.minLength(3)]],
+      'password': ['', [Validators.required, Validators.minLength(8), Validators.pattern('^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]+)$')]],
+      'email': ['' , [Validators.required, Validators.email]],
+      'firstname': ['' , Validators.required],
+      'lastname': [''],
+      'address': _FB.group({
+        'street': [''],
+        'city': ['', Validators.required],
+        'state': [''],
+        'zip': ['']
+      }),
+      'phone': _FB.array([])
+      }
+    );
+  }
+
+  ngOnInit(): void {
+        throw new Error('Method not implemented.');
+    }
 
 }
